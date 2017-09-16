18028b03f4d0bf2b8dae85a659de500f70b75de3
Guilherme Schults <schults125@gmail.com>
2017-09-16 19:26:20 -0300

    Delete calculaImc.js

--- calculaImc.js
+++
@@ -1,8 +0,0 @@
-var botao = document.getElementById("calcula-imcs");
-
-botao.addEventListener ("click", function () {
-	var trPacientes = document.getElementsByClassName("paciente");
-	montaPaciente(trPacientes);
-
-});
-
